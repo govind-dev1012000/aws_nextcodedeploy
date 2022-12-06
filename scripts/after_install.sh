4f98590c969de55ed14177b0c2d0eb37d42e53f0
Govindaraj-B <govi1012000@gmail.com>
2022-12-06 17:38:22 +0530

    firsttry

---
+++ scripts/after_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu/app
+sudo npm install
+sudo npm run build
---
+++ scripts/before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo mkdir -p /home/ubuntu/app
+sudo curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
