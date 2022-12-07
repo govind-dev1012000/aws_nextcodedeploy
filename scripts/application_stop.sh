a414878c54a447a22e200972cc3e5e8ca3288e13
Govindaraj-B <govi1012000@gmail.com>
2022-12-07 14:18:31 +0530

    all1

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
-# sudo systemctl stop myApp
+IS_ACTIVE=$(sudo systemctl is-active myApp)
+if [ "$IS_ACTIVE" == "active" ]; then
+    # restart the service
+    echo "Service is running"
+    echo "Restarting service"
+    sudo systemctl stop myApp
+    echo "Service restarted"
 
 # pm2 stop nextapp
